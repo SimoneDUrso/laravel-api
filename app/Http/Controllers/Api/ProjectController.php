0ddb10ee58174f92998a97ed7c5cb1bd738327c2
SimoneDUrso <simone.durso98@gmail.com>
2024-10-16 15:17:28 +0200

    [UPD] Added show function and new route in api.php

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,4 +17,20 @@ class ProjectController extends Controller
             'results' => $projects
         ]);
     }
+
+    public function show($slug)
+    {
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'results' => $project
+            ]);
+        }
+
+        return response()->jason([
+            'success' => false
+        ]);
+    }
 }
