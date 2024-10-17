f686d6f5e7342d49f763895fde77e20cb70a21e5
SimoneDUrso <simone.durso98@gmail.com>
2024-10-17 15:43:25 +0200

    [UPD] Created mailable with construct and envelope functions

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+class LeadController extends Controller
+{
+    public function store(Request $request) {}
+}
